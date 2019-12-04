54e5a4845965a885d22e871b7ffc96fd74364b67
mrvdvr <merveokur1987@gmail.com>
2019-12-04 20:53:10 +0300

    with React

---
+++ react-intro/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => { 
+    return (
+        <footer className="container">
+          <div>
+    <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+
+          </div>
+    
+  </footer>
+   
+    );
+};
+
+export default Footer;
